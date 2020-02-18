412d88d5f896d4bc32e33d762c52c4d78326b52e
Guido Di Natale <g.dinatale@buenosaires.gob.ar>
2020-02-18 11:45:06 -0300

    Fix demo

---
+++ demo/react-demo/src/App.js
@@ -0,0 +1,144 @@
+import React, { Component } from 'react';
+import { Autocompleter } from '@usig-gcba/autocompleter';
+import Geocoder from './utils/Geocoder';
+import './App.css';
+
+class App extends Component {
+  state = {
+    autocompleter: null,
+    showMap: false,
+    loading: false,
+    x: null,
+    y: null,
+    input: '',
+    error: null,
+    suggestions: [],
+    selectedSuggestion: null
+  };
+
+  handleInputChange = event => {
+    const text = event.target.value;
+    this.state.autocompleter.updateSuggestions(text);
+    this.setState({ input: text, showMap: false });
+  };
+
+  handleClick = suggestion => {
+    if (suggestion) {
+      this.setState({ selectedSuggestion: suggestion });
+      if (suggestion.type === 'CALLE') {
+        this.setState({
+          suggestions: [],
+          input: suggestion.title + ' '
+        });
+      } else {
+        this.setState({
+          input: suggestion.title,
+          suggestions: [],
+          loading: true
+        });
+        Geocoder.fetchGeolocation(suggestion)
+          .then(coords => {
+            this.setState({
+              showMap: true,
+              x: coords.x,
+              y: coords.y,
+              loading: false
+            });
+          })
+          .catch(err => {
+            console.error(err);
+            this.setState({ loading: false });
+          });
+      }
+    }
+  };
+
+  componentDidMount() {
+    // Opciones de config del autocomplete
+    const options = { maxSuggestions: 10, debug: false };
+    const buscarDireccionesAmba = false;
+
+    //Callbacks del autocomplete
+    const suggestionsCallback = suggestions => {
+      this.setState({ suggestions: suggestions });
+    };
+
+    const completeSuggestionsCallback = suggestions => {
+      if (suggestions.length === 0) {
+        this.setState({ suggestions: [] });
+      } else {
+        this.setState({ error: null });
+      }
+    };
+
+    const errorCallback = error => {
+      this.setState({ error: error });
+    };
+
+    const autocompleter = new Autocompleter(
+      {
+        onCompleteSuggestions: completeSuggestionsCallback,
+        onSuggestions: suggestionsCallback,
+        onError: errorCallback
+      },
+      options
+    );
+
+    autocompleter.addSuggester('Direcciones', { inputPause: 250 });
+    autocompleter.addSuggester('Lugares');
+    if (buscarDireccionesAmba) autocompleter.addSuggester('DireccionesAMBA');
+
+    this.setState({ autocompleter: autocompleter, suggestions: [] });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="section">
+          <div id="header">
+            <div id="logo">
+              <h1>USIG AutoCompleter (Demo)</h1>
+            </div>
+          </div>
+          Lugar{' '}
+          <input id="search-input" value={this.state.input} onChange={this.handleInputChange} />
+          <span id="ejemplo">ej.: Callao y Corrientes, Florida 550, Teatro San Martín, etc.</span>
+          {this.state.error ? this.state.error.message : null}
+          {this.state.suggestions.map((suggestion, index) => {
+            const title = suggestion.alias || suggestion.title || suggestion.nombre;
+            const subTitle = suggestion.subTitle ? suggestion.subTitle : suggestion.descripcion;
+            return (
+              <div className="sugerencia" key={index} onClick={() => this.handleClick(suggestion)}>
+                <div
+                  className="titulo-sugerencia"
+                  id={title + '-title-sug-' + index}
+                  aria-hidden="true"
+                >
+                  {title}
+                </div>
+                <div className="clase" id={subTitle + '-subtitle-sug-' + index} aria-hidden="true">
+                  {subTitle}
+                </div>
+              </div>
+            );
+          })}
+          {this.state.selectedSuggestion ? (
+            <div style={{ zIndex: 0, display: 'inline-block', maxWidth: '35 vw' }}>
+              <pre>{JSON.stringify(this.state.selectedSuggestion, null, 2)}</pre>
+            </div>
+          ) : null}
+          {this.state.loading ? 'cargando ...' : null}
+          {this.state.showMap ? (
+            <img
+              style={{ display: 'inline-block', maxWidth: '35 vw' }}
+              alt="mapa"
+              src={`http://servicios.usig.buenosaires.gob.ar/LocDir/mapa.phtml?x=${this.state.x}&y=${this.state.y}&w=600&punto=1&desc=`}
+            />
+          ) : null}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
