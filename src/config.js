8a89d2b6d0fe1639d6646d906055a05dcb884aad
Daniel De Campos <djosedecampos@buenosaires.gob.ar>
2020-05-14 12:59:23 -0300

    Se agrego url de Catastro y Lugares

--- src/config.js
+++ src/config.js
@@ -1,1 +1,3 @@
 export const usig_webservice_url = 'https://servicios.usig.buenosaires.gob.ar';
+export const catastro_webservice_url = 'https://epok.buenosaires.gob.ar/catastro';
+export const lugares_webservice_url = 'https://epok.buenosaires.gob.ar/getObjectContent';
